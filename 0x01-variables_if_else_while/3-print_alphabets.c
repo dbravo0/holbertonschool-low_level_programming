b7c6098c33f2e5edf081bbee051dbb68c6cf7bf7
David <stevenbravobel@gmail.com>
2019-06-20 20:37:23 +0000

    Added file 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+		putchar(a);
+		++a;
+	}
+	for (a = 65 ; a <= 90 ; ++a)
+	{
+		putchar(a);
+	}
+
+	putchar(10);
+
+	return (0);
+}
