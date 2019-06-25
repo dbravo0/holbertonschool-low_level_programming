72ade50365dc0c4f6b223220e629ed07747e8459
David <stevenbravobel@gmail.com>
2019-06-25 20:30:10 +0000

    Fixed file 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,20 @@
 #include "holberton.h"
 
 /**
- * _abs - Entry point
+ * _abs - computes the absolute value of a variable
+ *@v: The value to print
  *
+ *Return: Always 0
  */
+
 int _abs(int v)
 {
+	int d;
 
 	if (v < 0)
 	{
-		v = -100 * -1;
+		d = v * -1;
+		return (d);
 	}
 	return (0);
 }
