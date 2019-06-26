d062b9dc5d609713817240650ac68fc9500e3cc4
David <stevenbravobel@gmail.com>
2019-06-26 05:27:37 +0000

    Added file 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Adds two integers and return the result
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: The sum of the two parameters
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
