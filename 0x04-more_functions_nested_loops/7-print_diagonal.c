1489e32761bd667e3b4a2db3e19b54afe2ed2b80
David <stevenbravobel@gmail.com>
2019-06-28 03:41:27 +0000

    Added file 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Draws a diagonal line in the terminal
+ * @n: number of times of \
+ */
+void print_diagonal(int n)
+{
+	int d, l;
+
+	if (n > 0)
+	{
+		for (d = 0 ; d < n ; d++)
+		{
+			for (l = 0 ; l < d ; l++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
