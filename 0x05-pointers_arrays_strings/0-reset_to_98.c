bfe7912340b36e91f670bbd14ca1eb0c696e2d46
David <stevenbravobel@gmail.com>
2019-07-02 15:22:06 +0000

    Fixed file 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,10 @@
 #include "holberton.h"
 
+/**
+ * reset_to_98 - Takes a pointer and updates the value of n to 98
+ * @n: The number to be checked
+ */
 void reset_to_98(int *n)
 {
-	
-	
+	*n = 98;
+}
