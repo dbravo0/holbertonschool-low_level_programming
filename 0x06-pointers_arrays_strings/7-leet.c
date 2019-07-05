fc4cf9bbd85cdfd29e02d514b22933bd387555f9
David <stevenbravobel@gmail.com>
2019-07-05 04:31:49 +0000

    Added file 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,10 +2,29 @@
 
 /**
  * leet - encodes a string into 1337
+ * @l: String to be encoded
  *
  * Return: The encoded value
  */
-char *leet(char *)
+char *leet(char *l)
 {
+	int c;
+	int b;
+	int a[10] = {'a', 'e', 'o', 't', 'I', 'A', 'E', '0', 'T', 'L'};
+	int n[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
 
+	for (c = 0 ;  l[c] != '\0' ; c++)
+	{
+
+		for (b = 0 ; b < 10 ; b++)
+		{
+
+			if (l[c] == a[b])
+			{
+				l[c] = n[b];
+			}
+		}
+	}
+
+	return (l);
 }
