e6f1ee224818ec8e101012b1b49088f48d0293ff
David <stevenbravobel@gmail.com>
2019-07-11 02:42:48 +0000

    Fix file 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,12 +2,24 @@
 
 /**
  * _pow_recursion - Returns the value of x raised to the power of y
- * @x: ...
- * @y: ...
+ * @x: Base value of power
+ * @y: Exponential value of power
  *
  * Return: 0 for the lenght of thr string
  */
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+	{
 
+		return (-1);
+	}
+	else if (y == 0)
+	{
+
+		return (1);
+	}
+
+	y--;
+	return (x * _pow_recursion(x, y));
 }
