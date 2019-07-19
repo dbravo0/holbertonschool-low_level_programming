cc448965d76b58f45f7eb86d7748f304d831b462
David <stevenbravobel@gmail.com>
2019-07-19 02:47:34 +0000

    [Feature] file 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * *_calloc - Allocates memory for an array, using malloc
+ * @nmemb: Array of elements
+ * @size: Size of data bytes
+ *
+ * Return: Pointer void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	unsigned int i;
+	char *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0 ; nmemb * size ; i++)
+		s[i] = 0;
+
+	return (s);
+
+}
