df1258da8239a0ba1bd189dfddbc78c37ba16c33
David <stevenbravobel@gmail.com>
2019-07-23 22:18:57 +0000

    [Feature] Add file 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees dogs
+ * @d: Pointer dog
+ *
+ **/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
