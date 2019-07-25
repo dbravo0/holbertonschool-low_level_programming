c9952b60b8019d52a40ed058acbccb59d2dab47f
David <stevenbravobel@gmail.com>
2019-07-25 01:13:48 +0000

    [Feature] File 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - Prints a name
+ * @name: Name for be printed
+ * @f: Pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if ((f != NULL) && (name != NULL))
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNTION_POINTERS
+#define FUNTION_POINTERS
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
