415688987e8793daa9c7c406e02326d89a46917b
David <stevenbravobel@gmail.com>
2019-07-25 03:11:13 +0000

    [Fix] File 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,12 +8,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-        size_t c;
+	size_t c;
 
-        if ((array != NULL) && (action != NULL))
-        {
-                for (c = 0 ; c < size ; c++)
-                        action(array[c]);
-        }
+	if ((array != NULL) && (action != NULL))
+	{
+		for (c = 0 ; c < size ; c++)
+			action(array[c]);
+	}
 }
 
