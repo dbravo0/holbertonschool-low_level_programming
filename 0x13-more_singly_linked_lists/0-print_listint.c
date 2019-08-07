7d64fecc60a9f49e295a900201ce1ffbd248da7b
David <stevenbravobel@gmail.com>
2019-08-07 04:14:01 +0000

    [Fix] file 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,7 +4,7 @@
  * print_listint - Prints all the elements of a listint_t list
  * @h: Sizes of nodes
  *
- * return: Number of nodes
+ * Return: Number of nodes
  **/
 size_t print_listint(const listint_t *h)
 {
