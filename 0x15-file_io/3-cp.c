cecb77d7d5e1b2f6847c95f7c2e2d99f4a26e16d
David <stevenbravobel@gmail.com>
2019-08-18 04:58:39 +0000

    [Fix] file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,7 +6,7 @@
  *
  * Return: Always 0 (Success)
  **/
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int fd0, fd1;
 	char bf[1024];
@@ -18,22 +18,18 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd0 = open(argv[1], O_RDONLY);
-	if (fd0 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd1 == -1)
+	if (fd0 == -1 || fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write from a file %s\n", argv[2]);
 		exit(98);
 	}
 	while ((wr = read(fd0, bf, 1024)) > 0)
 	{
-		if (write(fd1, bf, wr) != wr)
+		if (write(fd1, bf, wr) != wr || fd1 < 0)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
+			close(fd0);
 			exit(99);
 		}
 	}
