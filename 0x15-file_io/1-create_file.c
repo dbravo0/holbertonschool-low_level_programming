ec2f6f998ae73e7831123e142e3aeabd9ae1a2c9
David <stevenbravobel@gmail.com>
2019-08-18 03:11:42 +0000

    [Fix] file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,17 +3,32 @@
 /**
  * create_file - Creates a file
  * @filename: Name of file for I/O
- * @text_content: ...
+ * @text_content: Content of file
  *
- * Return: ...
+ * Return: 1 if success, -1 if not
  **/
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, wr;
 
 	if  (filename == NULL)
 		return (-1);
 
-	fd = open (filename, O_CREATE, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	for (wr = 0 ; text_content[wr] ; wr++)
+		;
+
+	wr = write(fd, text_content, wr);
+	if (wr == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
 
 }
