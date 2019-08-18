d20748765df7c4acb20220953a57acb5dca28716
David <stevenbravobel@gmail.com>
2019-08-18 04:23:49 +0000

    [Fix] file 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * main - Main funtion
  * @argc: Argument count
@@ -10,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	int fd0, fd1;
-	char bf [1024];
+	char bf[1024];
 	ssize_t wr;
 
 	if (argc != 3)
@@ -24,14 +23,12 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd1 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write from a file %s\n", argv[2]);
 		exit(98);
 	}
-
 	while ((wr = read(fd0, bf, 1024)) > 0)
 	{
 		if ((write(fd1, bf, wr)) != wr)
@@ -50,6 +47,5 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
 		exit(98);
 	}
-
 	return (0);
 }
