405a66accb980795e4d4c2d545fd7ff49ca2d487
David <stevenbravobel@gmail.com>
2019-09-16 05:24:56 +0000

    [Feature] files 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -13,11 +13,11 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h == NULL)
 		return (0);
 
-	for (n = 0 ;  ; n++)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		n++;
 	}
-
 	return (n);
 }
