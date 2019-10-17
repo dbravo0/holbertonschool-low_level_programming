f0ec692636913aa48a2894dc92470407a8bb5f38
David <stevenbravobel@gmail.com>
2019-10-17 06:54:06 +0000

    [Fix] file 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,13 +11,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 
-	hash_node_t *hash_node;
+	hash_node_t *hash_node, *c;
 	unsigned long int hash_index;
 
 	if (!ht || !key || !value)
 		return (0);
 
 	hash_index = key_index((const unsigned char *)key, ht->size);
+
+	c = ht->array[hash_index];
+
+	while (c)
+	{
+		if (strcmp(curr->key, key) == 0)
+		{
+			free(c->value);
+			c->value = strdup(value);
+
+			if (!c->value)
+				return (0);
+
+			return (1);
+		}
+		c = c->next;
+	}
 
 	hash_node = malloc(sizeof(hash_node_t));
 
